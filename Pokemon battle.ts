17c44045eb33a73689e3a6967183f1db71adf785
JinxCY93 <vincentjinx@hotmail.com>
2019-03-23 11:19:39 +0800

    Scenario 1 done

--- Pokemon battle.ts
+++ Pokemon battle.ts
@@ -1,57 +1,191 @@
 import { question } from 'readline-sync'
-
-const opponentMob = 'Mr. Mime'
-let opponentHP = 200
-const oppSkills = ['Stun', 'Paralyze', 'Body Slam']
-const oppSkillsDmg = [30, 40, 50]
-let isMyTurn = true
-
-let myMob = {
-  Name: 'Gangar',
-  HP: 300
+let currentdmg = 0
+let opponentMob = {
+  oname: 'Mr. Mime',
+  ohp: 200,
+  odef: 10,
+  oatk: 10
 }
-
-const myMobSkills = [
+const oppSkills = [
   {
-    name: 'Shadow ball',
-    damage: 50
+    osname: 'Atk',
+    osdamage: opponentMob.oatk,
+    ostype: 'physical'
   },
   {
-    name: 'Thunderstrike',
-    damage: 60
+    osname: 'Stun',
+    osdamage: 30,
+    ostype: 'armorbreak'
   },
   {
-    name: 'Physical Damage',
-    damage: 60
+    osname: 'Paralyze',
+    osdamage: 40,
+    ostype: 'electric'
+  },
+  {
+    osname: 'Body Slam',
+    osdamage: 50,
+    ostype: 'physical'
+  },
+  {
+    osname: 'Harden',
+    osdamage: 10,
+    ostype: 'physical'
   }
 ]
 
+let isMyTurn = true
+let myMob = {
+  Name: 'Gangar',
+  HP: 300,
+  Def: 20,
+  Atk: 40
+}
+const myMobSkills = [
+  {
+    sname: 'Atk',
+    damage: myMob.Atk,
+    type: 'physical'
+  },
+  {
+    sname: 'Shadow ball',
+    damage: 50,
+    type: 'Dark'
+  },
+  {
+    sname: 'Blackhole',
+    damage: 60,
+    type: 'Dark'
+  },
+  {
+    sname: 'Tackle',
+    damage: 40,
+    type: 'armorbreak'
+  },
+  {
+    sname: 'Tail Whip',
+    damage: 5,
+    type: 'armorbreak'
+  }
+]
+
+function reducephysicaldamage(a, b) {
+  return a - b
+}
+
+function ABdamage(a, b) {
+  return (a * 1.5) - b
+}
+
 function displaySkills(array) {
   let i = 0
-  while (i < 3) {
-    console.log(i + '. ' + array[i].name + ', Damage:' + array[i].damage)
+  while (i < 5) {
+    console.log(i + '. ' + array[i].sname + ', Damage:' + array[i].damage)
 
     // i += 1
     i = i + 1
   }
 }
 
-console.log('You have encounter ' + opponentMob + '.')
-console.log('You send in ' + myMob[0] + '.')
-console.log(myMob[0] + ' : ' + myMob[1])
-console.log(opponentMob + ' : ' + opponentHP)
-console.log("You can choose one of " + myMob + "'s skills.")
+console.log('You have encounter ' + opponentMob.oname + '.')
+console.log('You send in ' + myMob.Name + '.')
+console.log(myMob.Name + ' health : ' + myMob.HP)
+console.log(opponentMob.oname + ' health : ' + opponentMob.ohp)
+console.log("You can choose one of " + myMob.Name + "'s skills.")
 
-let myMobHP = myMob[1]
-while (myMob.HP > 0 && opponentHP > 0) {
+while (myMob.HP > 0 && opponentMob.ohp > 0) {
   console.log('-------------------------------------------------------------')
-  let attackername = isMyTurn ? myMob.Name : opponentMob
-  let defendername = isMyTurn ? opponentMob : myMob.Name
-  displaySkills(myMobSkills)
-  let selectedSkills = question('Select a Skill: ')
+  let attackername = isMyTurn ? myMob.Name : opponentMob.oname
+  let defendername = isMyTurn ? opponentMob.oname : myMob.Name
 
-  console.log(attackername + ' use ' + skills[Num] + '. ' + skills[Num] + ' hits for ' + dmg[Num] + ' DMG.')
-  let currentdmg = dmg[Num]
+  if (isMyTurn) {
+    displaySkills(myMobSkills)
+    let selectedSkills = question('Select a Skill: ')
+    console.log(attackername + ' use ' + myMobSkills[selectedSkills].sname + '. ' + myMobSkills[selectedSkills].sname + ' hits for ' + myMobSkills[selectedSkills].damage + ' DMG.')
+    if (myMobSkills[selectedSkills].type === 'physical') {
+      currentdmg = reducephysicaldamage(myMobSkills[selectedSkills].damage, opponentMob.odef)
+      if (currentdmg < 0) {
+        currentdmg = 0
+        console.log('Your physical damage dropped to ' + currentdmg + ' due to ' + opponentMob.oname + ' physical defense.')
+        opponentMob.ohp = opponentMob.ohp - currentdmg
+        console.log('Not Effective. ' + defendername + ' health not reduce. HP remained  ' + opponentMob.ohp + '.')
+        console.log('-------------------------------------------------------------')
+      } else {
+        console.log('Your physical damage dropped to ' + currentdmg + ' due to ' + opponentMob.oname + ' physical defense.')
+        opponentMob.ohp = opponentMob.ohp - currentdmg
+        console.log('Critical Hit. ' + defendername + ' health reduced to ' + opponentMob.ohp + '.')
+        console.log('-------------------------------------------------------------')
+      }
+    } else if (myMobSkills[selectedSkills].type === 'armorbreak') {
+      currentdmg = ABdamage(myMobSkills[selectedSkills].damage, opponentMob.odef)
+      if (currentdmg < 0) {
+        currentdmg = 0
+        console.log('Your armor break skill dealed ' + currentdmg + ' damage.')
+        opponentMob.ohp = opponentMob.ohp - currentdmg
+        console.log('Not Effective. ' + defendername + ' health not reduce. HP remained  ' + opponentMob.ohp + '.')
+        console.log('-------------------------------------------------------------')
+      } else {
+        console.log('Your armor break skill dealed ' + currentdmg + ' damage.')
+        opponentMob.ohp = opponentMob.ohp - currentdmg
+        console.log('Critical Hit. ' + defendername + ' health reduced to ' + opponentMob.ohp + '.')
+        console.log('-------------------------------------------------------------')
+      }
+    }
+    else {
+      opponentMob.ohp = opponentMob.ohp - myMobSkills[selectedSkills].damage
+      console.log('Critical Hit. ' + defendername + ' health reduced to ' + opponentMob.ohp + '.')
+      console.log('-------------------------------------------------------------')
+    }
 
+  } else {
+    const Num = Math.floor(Math.random() * 5)
+    console.log(attackername + ' use ' + oppSkills[Num].osname + '. ' + oppSkills[Num].osname + ' hits for ' + oppSkills[Num].osdamage + ' DMG.')
+    if (oppSkills[Num].ostype === 'physical') {
+      currentdmg = reducephysicaldamage(oppSkills[Num].osdamage, myMob.Def)
+      if (currentdmg < 0) {
+        currentdmg = 0
+        console.log('Enemy damage dropped to ' + currentdmg + ' due to ' + myMob.Name + ' physical defense.')
+        myMob.HP = myMob.HP - currentdmg
+        console.log('Not Effective. ' + defendername + ' health not reduce. HP remained ' + myMob.HP + '.')
+        console.log('-------------------------------------------------------------')
+      }
+      else {
+        console.log('Enemy damage dropped to ' + currentdmg + ' due to ' + myMob.Name + ' physical defense.')
+        myMob.HP = myMob.HP - currentdmg
+        console.log('Critical Hit. ' + defendername + ' health reduced to ' + myMob.HP + '.')
+        console.log('-------------------------------------------------------------')
+      }
+    } else if (oppSkills[Num].ostype === 'armorbreak') {
+      currentdmg = ABdamage(oppSkills[Num].osdamage, myMob.Def)
+      if (currentdmg < 0) {
+        currentdmg = 0
+        console.log('Enemy armor break skill dealed ' + currentdmg + ' damage.')
+        myMob.HP = myMob.HP - currentdmg
+        console.log('Not Effective. ' + defendername + ' health not reduce. HP remained ' + myMob.HP + '.')
+        console.log('-------------------------------------------------------------')
+      } else {
+        console.log('Enemy armor break skill dealed ' + currentdmg + ' damage.')
+        myMob.HP = myMob.HP - currentdmg
+        console.log('Critical Hit. ' + defendername + ' health reduced to ' + myMob.HP + '.')
+        console.log('-------------------------------------------------------------')
+      }
+    }
+    else {
+      myMob.HP = myMob.HP - oppSkills[Num].osdamage
+      console.log('Critical Hit. ' + defendername + ' health reduced to ' + myMob.HP + '.')
+      console.log('-------------------------------------------------------------')
+    }
+  }
   isMyTurn = !isMyTurn
+}
+
+if (opponentMob.ohp <= 0) {
+  console.log(opponentMob.oname + ' has fainted.')
+  console.log('You have gained 50 exp points.')
+  console.log('Game over!')
+}
+
+else {
+  console.log(myMob.Name + ' has fainted.')
+  console.log('Better luck next time...')
 }
